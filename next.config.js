367864babbb4b1bb1ef4e9a64e3bd951d2bc8751
shaggy3006 <hichemmahiout11@gmail.com>
2026-05-28 17:19:35 +0100

    Fix basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? "/hr-wear" : "",
+  assetPrefix: isProd ? "/hr-wear/" : "",
 };
 
 module.exports = nextConfig;
